a4ff3ce581034982487f7a2e8e97012f24614847
Chanple <chanple@outlook.com>
2018-03-21 22:41:27 +0800

    ~

---
+++ DigitalClock/DigitalClock/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Interop;
+
+namespace DigitalClock
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new TimeModel();
+            Left = (SystemParameters.PrimaryScreenWidth - Width) * 3 / 4;
+            Top = 0;
+        }
+    }
+}
---
+++ DigitalClock/DigitalClock/TimeModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ComponentModel;
+using System.Timers;
+
+namespace DigitalClock
+{
+    class TimeModel : INotifyPropertyChanged
+    {
+        private const string Format = "d2";
+        private Timer timer;
+        private DateTime now;
+
+        public TimeModel()
+        {
+            timer = new Timer(1000);
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            now = DateTime.Now;
+            Hour = now.Hour.ToString(Format);
+            Minute = now.Minute.ToString(Format);
+            Second = now.Second.ToString(Format);
+        }
+
+        private string _hour;
+        private string _minute;
+        private string _second;
+
+        public string Hour
+        {
+            get => _hour; set
+            {
+                if (_hour != value)
+                {
+                    _hour = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Hour"));
+                }
+            }
+        }
+
+        public string Minute
+        {
+            get => _minute; set
+            {
+                if (_minute != value)
+                {
+                    _minute = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Minute"));
+                }
+            }
+        }
+
+        public string Second
+        {
+            get => _second; set
+            {
+                _second = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Second"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
